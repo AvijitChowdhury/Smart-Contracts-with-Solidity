ecd052c14fc98cae0e4476b0ed7b2d6d167f71f7
Avijit Chowdhury <abhichy30@gmail.com>
2023-04-07 02:02:23 +0600

    solidity for smart contracts

---
+++ contracts/learn/datatypes.sol
@@ -0,0 +1,14 @@
+pragma solidity >=0.8.4 <=0.9.4;
+
+contract DataTypeContract{
+
+    uint8 num8 = 8;
+    uint16 num16=1234;
+    uint256 num256 = 12344545;
+
+
+    int negPos = -34231;
+
+    bool df= true;
+
+}
